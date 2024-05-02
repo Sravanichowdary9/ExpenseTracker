1a9be0a8d59b725c9d0b84e7856869d4f2f4d10d
Amrita <amritamanoj899@gmail.com>
2024-05-02 17:56:08 +0800

    Adding HTML and CSS for Sign Up form and changes to Login Page

--- loginform.js
+++ loginform.js
@@ -1,13 +1,46 @@
-function togglePassword() {
-    var x = document.getElementById("pwd");
-    var y = document.getElementById("toggleIcon");
-    if (x.type === "password") {
-        x.type = "text";
-        y.classList.add("bi-eye");
-        y.classList.remove("bi-eye-slash");
+function togglePassword(inputId, iconId) {
+    var input = document.getElementById(inputId);
+    var icon = document.getElementById(iconId);
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("bi-eye-slash");
+        icon.classList.add("bi-eye");
     } else {
-        x.type = "password";
-        y.classList.add("bi-eye-slash");
-        y.classList.remove("bi-eye");
+        input.type = "password";
+        icon.classList.remove("bi-eye");
+        icon.classList.add("bi-eye-slash");
+    }
+}
+
+function validateDOB() {
+    var dobInput = document.getElementById('dob');
+    var dob = new Date(dobInput.value);
+    var today = new Date();
+    today.setHours(0, 0, 0, 0); 
+
+    if (dob > today) {
+        alert('Please check your Date of Birth!');
+        dobInput.value = ''; 
+    }
+}
+
+function validatePasswords() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    if (password !== confirmPassword) {
+        alert("Passwords do not match.");
+        return false; 
+    }
+    return true;
+}
+
+function checkPasswordsMatch() {
+    var password = document.getElementById("password").value;
+    var confirmPassword = document.getElementById("confirmPassword").value;
+    var message = document.getElementById("passwordMatchMessage");
+    if (password !== confirmPassword) {
+        message.textContent = "Passwords do not match.";
+    } else {
+        message.textContent = ""; 
     }
 }
