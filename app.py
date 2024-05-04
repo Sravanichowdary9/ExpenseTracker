d0954a9ad5128d4b2f9f4e81b87f51ead75c41d9
Amrita <amritamanoj899@gmail.com>
2024-05-04 22:20:44 +0800

    Integrating Flask and added CSS for Login and Signup pages.

--- app.py
+++ app.py
@@ -1,92 +1,49 @@
-from flask import Flask, render_template, redirect, url_for
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length
-from flask_sqlalchemy import SQLAlchemy
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'NOBODY-CAN-GUESS-THIS'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\sqlite\\database.db'
-Bootstrap(app)
-db = SQLAlchemy(app)
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = 'login'
-
-
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20))
-    email = db.Column(db.String(30), unique=True)
-    password = db.Column(db.String(80))
-
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
-class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=80)])
-    remember = BooleanField('remember me')
-
-
-class RegisterForm(FlaskForm):
-    email = StringField('email', validators=[InputRequired(), Email(message="Invalid Email"), Length(min=6, max=30)])
-    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
-    password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=80)])
-
 
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    return render_template('login.html')
 
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user:
-            # compares the password hash in the db and the hash of the password typed in the form
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('dashboard'))
-        return 'invalid username or password'
+@app.route('/perform_login', methods=['POST'])
+def perform_login():
+    username = request.form['username']
+    password = request.form['password']
 
-    return render_template('login.html', form=form)
+    if username == "amrita" and password == "123":
+        return "Login Successful!"
+    else:
+        return "Invalid credentials", 401
 
-
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup')
 def signup():
-    form = RegisterForm()
+    return render_template('signup.html')
 
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        # add the user form input which is form.'field'.data into the column which is 'field'
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
-        return 'new user has been created bro!'
+@app.route('/forgot_password')
+def forgot_password():
+    return "Forgot password page under construction"
 
-    return render_template('signup.html', form=form)
+@app.route('/submit-signup', methods=['POST'])
+def submit_signup():
+    firstName = request.form['firstName']
+    middleName = request.form['middleName']
+    lastName = request.form['lastName']
+    dob = request.form['dob']
+    email = request.form['email']
+    mobile = request.form['mobile']
+    country = request.form['country']
+    password = request.form['password']  
+    confirmPassword = request.form['confirmPassword']  
+    gender = request.form['gender']
+    
+    return "Signup successful! Welcome, {}".format(firstName)
 
 
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html', name=current_user.username)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
+@app.errorhandler(Exception)
+def handle_exception(e):
+    print(e)
+    return str(e), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
